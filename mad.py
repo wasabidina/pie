4e564bb36285f3e83aab8c0506306ab1b1106e83
wasabidina <iman.madina96@mail.ru>
2021-04-03 15:52:41 +0600

    first commit

---
+++ mad.py
@@ -0,0 +1,35 @@
+#print('hello Madina')
+#print('Hello Arkhat')
+
+
+# x = 5
+# y = 7
+# print(x)
+# print(x,y)
+# print(x+y)
+# name = "Madina"
+# #print(name)
+# surname = "Imangaliyeva"
+# print(name,surname) 
+# if 6==6:
+# 	print(x,y,'matematika')
+	# a = "string Madina"
+	# b = 77 #integer
+	# c = 5.12 #float
+	# list1 = [1,2,'ASdsd']
+	# dictionary = {
+	#  	"a" : "asd",
+	#  	"b" : 123
+	# }
+	# boolean = True #False
+
+a = '1'
+# a = float(a)
+b = 2.912
+# b = float(b)
+c = 3
+# c = float(c)
+
+# print(float(a) + float(b) + float(c))
+# print(str(a) + str(b) + str(c))
+print(int(a) + int(round(b)) + int(c))
